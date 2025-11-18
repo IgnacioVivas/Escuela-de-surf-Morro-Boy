d4b8fb5372b6afad1d593975b8809964940460d5
IgnacioVivas <ignacab@hotmail.com>
2025-11-18 18:00:34 -0300

    primer commit

--- src/components/sections/Banner.tsx
+++ src/components/sections/Banner.tsx
@@ -5,9 +5,13 @@ function Banner() {
 	return (
 		<div className="relative flex justify-center items-center ">
 			<img src={banner} alt="playa" />
-			<div className="absolute text-white">
-				<h2>Do you want to start surfing now?</h2>
-				<p>Get on Miami Beach waters! Enjoy a place with perfect surfing conditions and our professional trainers.</p>
+			<div className="absolute text-white flex flex-col justify-center items-center">
+				<h2 className="text-3xl text-center font-bold md:text-[40px] uppercase tracking-widest mb-4">
+					Do you want to <br /> start surfing now?
+				</h2>
+				<p className="text-[19px] text-center leading-relaxed">
+					Get on Miami Beach waters! Enjoy a place with perfect surfing conditions and our professional trainers.
+				</p>
 				<ButtonUno>STart now</ButtonUno>
 			</div>
 		</div>
