970e212981888ec23d12405f5d6a56d414ea854e
IgnacioVivas <ignacab@hotmail.com>
2025-11-18 17:50:34 -0300

    actualizando rama

--- src/components/sections/Banner.tsx
+++ src/components/sections/Banner.tsx
@@ -3,9 +3,9 @@ import ButtonUno from '../miUi/ButtonUno';
 
 function Banner() {
 	return (
-		<div className="relative">
-			<img src={banner} alt="" />
-			<div className="absolute">
+		<div className="relative flex justify-center items-center ">
+			<img src={banner} alt="playa" />
+			<div className="absolute text-white">
 				<h2>Do you want to start surfing now?</h2>
 				<p>Get on Miami Beach waters! Enjoy a place with perfect surfing conditions and our professional trainers.</p>
 				<ButtonUno>STart now</ButtonUno>
