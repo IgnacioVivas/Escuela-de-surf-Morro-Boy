373e4ca2bd6dbed1b9d426593cffeadf337217d9
IgnacioVivas <ignacab@hotmail.com>
2025-11-18 17:41:42 -0300

    creando banner

--- src/components/sections/Banner.tsx
+++ src/components/sections/Banner.tsx
@@ -1,9 +1,15 @@
 import banner from '@/assets/playa.png';
+import ButtonUno from '../miUi/ButtonUno';
 
 function Banner() {
 	return (
-		<div>
+		<div className="relative">
 			<img src={banner} alt="" />
+			<div className="absolute">
+				<h2>Do you want to start surfing now?</h2>
+				<p>Get on Miami Beach waters! Enjoy a place with perfect surfing conditions and our professional trainers.</p>
+				<ButtonUno>STart now</ButtonUno>
+			</div>
 		</div>
 	);
 }
