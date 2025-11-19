b02bedb2b7945e8e9c910f05ea9f80e303615d76
IgnacioVivas <ignacab@hotmail.com>
2025-11-19 01:21:58 -0300

    footer terminado desktop y mobile

--- src/components/miUi/Footer.tsx
+++ src/components/miUi/Footer.tsx
@@ -2,9 +2,14 @@ import { Facebook, Instagram, MapPin, Send, Smartphone, Waves } from 'lucide-rea
 import surfer from '@/assets/surfer.png';
 
 function Footer() {
+	const phone = '5493517713933';
+	const message = 'Olá! Quero começar as aulas de surf. Pode me passar mais informações?';
+
+	const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
 	return (
 		<div className="bg-[#0C0C0C]">
-			<div className="px-10 xl:px-30 py-28 grid grid-cols-1 xl:grid-cols-4 gap-10">
+			<div className="px-10 xl:px-30 py-28 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10">
 				<div className="flex flex-col gap-4">
 					<h1 className="text-white text-3xl font-dafoe tracking-widest font-bold uppercase">
 						Escola de surf Morro Boy
@@ -15,21 +20,25 @@ function Footer() {
 					</span>
 				</div>
 
-				<div className="flex flex-col gap-4">
+				<div className="flex flex-col gap-4 md:col-start-1 md:row-start-2 xl:col-start-2 xl:row-start-1">
 					<h3 className="text-white text-lg tracking-widest font-semibold uppercase flex items-center gap-2">
 						<Waves className="text-celeste" /> Redes sociais
 					</h3>
 					<div className="flex gap-2">
-						<a href="">
-							<span className="bg-naranja size-10 flex justify-center items-center rounded-full">
-								<Facebook color="white" size={25} />
-							</span>
+						<a
+							href="https://www.facebook.com/p/Morro-Boy-Escola-de-Surf-100070004620773/"
+							target="_blank"
+							className="bg-naranja size-10 flex justify-center items-center rounded-full"
+						>
+							<Facebook color="white" size={25} />
 						</a>
 
-						<a href="">
-							<span className="bg-naranja size-10 flex justify-center items-center rounded-full">
-								<Instagram color="white" size={25} />
-							</span>
+						<a
+							href="https://www.instagram.com/morroboyescoladesurf/"
+							target="_blank"
+							className="bg-naranja size-10 flex justify-center items-center rounded-full"
+						>
+							<Instagram color="white" size={25} />
 						</a>
 					</div>
 				</div>
@@ -39,18 +48,26 @@ function Footer() {
 						<Waves className="text-celeste" /> Entre em contato
 					</h3>
 					<div className="flex flex-col items-start gap-2 font-source text-[#adadad] text-base">
-						<span className="flex items-center justify-center gap-1 hover:text-celeste cursor-pointer">
+						<a
+							href={url}
+							target="_blank"
+							className="flex items-center justify-center gap-1 hover:text-celeste cursor-pointer"
+						>
 							<Smartphone color="white" size={14} />
 							<p>+55 12 XXXX-XXXX</p>
-						</span>
-						<span className="flex items-center justify-center gap-1 hover:text-celeste cursor-pointer">
+						</a>
+						<a target="_blank" className="flex items-center justify-center gap-1 hover:text-celeste cursor-pointer">
 							<Send color="white" size={14} />
 							<p>prueba@escuelasurf.com.br</p>
-						</span>
-						<span className="flex items-center justify-center gap-1 hover:text-celeste cursor-pointer">
+						</a>
+						<a
+							href="https://maps.app.goo.gl/MHGEskEDKTHYR6GBA"
+							target="_blank"
+							className="flex items-center justify-center gap-1 hover:text-celeste cursor-pointer"
+						>
 							<MapPin color="white" size={14} />
-							<p>Itamambuca – Ubatuba, SP</p>
-						</span>
+							<p>Praia do Itamambuca, Ubatuba - SP</p>
+						</a>
 					</div>
 				</div>
 
